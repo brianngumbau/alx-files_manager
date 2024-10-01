49743945d30b96f4d10c3280fc9e6cec337ed255
brianngumbau <brianngumbau9075@gmail.com>
2024-10-01 21:36:31 +0300

    fixing mongodb connection problem

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
-// import { MongoClient } from 'mongodb';
-import pkg from 'mongodb';
+import { MongoClient } from 'mongodb';
+// import pkg from 'mongodb';
 
-const { MongoClient } = pkg;
+// const { MongoClient } = pkg;
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
